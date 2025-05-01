651ad5419ceba8dda6e5f350a7df356b2bc3d682
ashishsinghAK <ashishksingh00110@gmail.com>
2025-05-02 00:51:47 +0530

    Routes changes

--- Route/SchoolRoute.js
+++ Route/SchoolRoute.js
@@ -1,7 +1,10 @@
 const express = require('express')
+
 const router = express.Router()
 
 const schoolController = require('../Controller/School_Controller')
+
+
 
 router.post("/addSchool",schoolController.addSchool)
 router.get("/listSchools",schoolController.listSchool)
